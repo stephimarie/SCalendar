8aaa13993c02ce23db138a271dbd77133b7f35b1
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-10-30 10:03:41 -0700

    Time slots and color change active

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
-// Today's date and current time
-moment().calendar(); 
+$(document).ready(function () {
 
-// Current time
-moment().subtract(10, 'days').calendar();
+    console.log(parseInt(moment().format('H')))
+  
+    const currentTime = moment().format('Do YYYY MMMM')
+    $("#currentTime").text(currentTime)
+    $("span").attr("style", "width: 75px")
+    $("button").text("Submit")
+  
+    const times = [21, 22, 23]
+  
+    times.forEach(time => {
+      const timeCheck = window.localStorage.getItem(time)
+  
+      const currentHour = moment().hour()
+  
+      console.log(currentHour)
+      console.log(time)
+  
+      if (currentHour > time) {
+        $(`#${time}`).addClass("bg-danger text-light")
+        $(`#${time}`).attr("disabled", true)
+      } else if (currentHour === time) {
+        $(`#${time}`).addClass("bg-secondary text-light")
+      } else {
+        $(`#${time}`).addClass("bg-success text-light")
+      }
+  
+      if (timeCheck === null) {
+        window.localStorage.setItem(time, "")
+      } else if (timeCheck.length > 0) {
+        $(`#${time}`).attr("value", window.localStorage.getItem(time))
+      }
+    })
+  
+  
+  
+  
+    $("form").on("submit", function (e) {
+      e.preventDefault()
+      const time = e.target.querySelector("input").getAttribute("id")
+      const text = e.target.querySelector("input").value
+  
+      window.localStorage.setItem(time, text)
+    })
+  })
+  
