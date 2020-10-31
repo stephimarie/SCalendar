de7d0f2fa73845c8eeb62ee61869f9d5fe02ffbd
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-10-30 19:59:28 -0700

    Fixed span on all time slots

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     $("button").text("Submit")
     // Work day
     const times = [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-  
+    // Time check in local storage 
     times.forEach(time => {
       const timeCheck = window.localStorage.getItem(time)
   
@@ -18,16 +18,21 @@ $(document).ready(function () {
   
       console.log(currentHour)
       console.log(time)
-  
+      
+      // If current hour is greater then time 
       if (currentHour > time) {
+        // Time will change color based on past
         $(`#${time}`).addClass("bg-danger text-light")
         $(`#${time}`).attr("disabled", true)
+        // If Time is current
       } else if (currentHour === time) {
         $(`#${time}`).addClass("bg-secondary text-light")
+        // IF Time is future
       } else {
         $(`#${time}`).addClass("bg-success text-light")
       }
-  
+      
+      // Set items to time - else if greater then 0
       if (timeCheck === null) {
         window.localStorage.setItem(time, "")
       } else if (timeCheck.length > 0) {
@@ -43,7 +48,7 @@ $(document).ready(function () {
       // jQuery for text input connected to time / Value typed
       const time = e.target.querySelector("input").getAttribute("id")
       const text = e.target.querySelector("input").value
-  
+      // Saved to local storage
       window.localStorage.setItem(time, text)
     })
   })
